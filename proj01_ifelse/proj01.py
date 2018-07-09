04e4e2c3cbb6b025667801252ba14ba87361fccd
ryanaspears <40898906+ryanaspears@users.noreply.github.com>
2018-07-09 13:34:00 -0500

    Add files via upload

--- proj01_ifelse/proj01.py
+++ proj01_ifelse/proj01.py
@@ -1,5 +1,5 @@
 # Name:
-# Date:
+# Date:7/9/18
 
 # proj01: A Simple Program
 
@@ -12,4 +12,33 @@
 # Then, it prints a sentence that says the number of days and months until their birthday
 
 
-# If you complete extensions, describe your extensions here!
+# If you complete extensions, describe your extensions here
+"""
+grade = raw_input("What grade are you in? ")
+name = raw_input("What is your name? ")
+years = 12 - int(grade)
+grad = str(years)
+name = name[0].upper() + name[1:].lower()
+print name + ", you are " + grad + " year(s) from graduation."
+current_day = 9
+current_month = 7
+month = raw_input("What is your birth month? (Numerical) ")
+month = int(month)
+if month >= current_month:
+    month = month - current_month
+elif month <= current_month:
+    month = 12 - (month - current_month)
+day = raw_input("What day were you born on? (Also numerical) ")
+day = int(day)
+if day >= current_day:
+    day = day - current_day
+elif day <= current_day:
+    day = 30 - current_day - day
+month = str(month)
+day = str(day)
+print "Your birthday is in " + month + " months and " + day + " days."
+"""
+age = raw_input("How old are you?")
+age = int(age)
+if 13 < age > 17:
+    print"You can watch "
