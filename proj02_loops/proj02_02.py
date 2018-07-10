97407fc6e20350b1b31436946f03f5ea6333751c
ryanaspears <40898906+ryanaspears@users.noreply.github.com>
2018-07-10 10:22:06 -0500

    Add files via upload

--- proj02_loops/proj02_02.py
+++ proj02_loops/proj02_02.py
@@ -9,5 +9,46 @@ sequence is a sequence of numbers where the next number in the sequence is the s
 previous two numbers in the sequence. The sequence looks like this: 
 1, 1, 2, 3, 5, 8, 13...
 """
+"""
+counter = 1
+input1 = raw_input("How many Fibonacci numbers would you like to generate? ")
+num_low = 0
+num_high = 1
+num_spare = 0
+print 1
+for counter in range (int(input1)):
+    print num_high + num_low
+    num_spare = num_high
+    num_high = num_high + num_low
+    num_low = num_spare
+"""
+
+# While loop version
+
+"""
+counter = 1
+input2 = raw_input("How many Fibonacci numbers would you like to generate? ")
+num_low = 0
+num_high = 1
+num_spare = 0
+print 1
+while counter < int(input1):
+    print num_high + num_low
+    num_spare = num_high
+    num_high = num_high + num_low
+    num_low = num_spare
+    counter = counter + 1
+"""
+
+# Powers of 2 version
+
+"""
+counter = 1
+input3 = raw_input("How many powers of 2 would you like to generate? ")
+for counter in range (1, int(input3) + 1):
+    print 2 ** counter
+"""
+
+# Divisors Version
 
 
